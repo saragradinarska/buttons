1ae024530809525ae00a8992dfb9a14849cdabfd
Sara Gradinarska <saragradinarska@Saras-MacBook-Air-8.local>
2019-11-11 18:03:56 +0800

    made changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [activeButton, setActiveButton] = useState("")
+
+  function handleClick(e){
+    let id = e.target.id;
+    setActiveButton(id);
+  }
+
+  //condition ? outcomeIfTrue : outcomeIfFalse
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button id="first"className={(activeButton == "first") ? "Active": ""}  onClick={handleClick}>First Button</button>
+    <button id="second"className={(activeButton == "second") ? "Active": ""} onClick={handleClick}>Second Button</button>
+    <button id="third"className={(activeButton == "third") ? "Active": ""} onClick={handleClick}>Third Button</button>
+    {activeButton}
+
     </div>
   );
 }
